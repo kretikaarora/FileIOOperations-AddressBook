730acc9277ace249fb63eb83afd57fd0de27bd2e
kretika arora <kretikaarora@gmail.com>
2020-10-29 14:14:49 +0530

    [Kretika Arora] Reading and Writting using csv files

--- FileIOoperation-AddressBook/ContactPerson.cs
+++ FileIOoperation-AddressBook/ContactPerson.cs
@@ -10,14 +10,14 @@ namespace AddressBookSystem
 /// </summary>
     public class ContactPerson
     {
-        public string firstName;
-        public string lastName;
-        public string address;
-        public string city;
-        public string state;
-        public int zip;
-        public double phoneNo;
-        public string email;
+        public string firstName { get; set;}
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zip { get; set; }
+        public double phoneNo { get; set; }
+        public string email { get; set; }
 
         /// <summary>
         /// Parameterised Constructor for Contact Person with below parameters
